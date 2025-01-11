eba467654bdc49cb7c4ebb730745a527d1d65616
jay <jay@jeepies.codes>
2025-01-11 23:18:12 +0000

    [JP-8] hf: rewrite test

--- tests/routes/_index.test.tsx
+++ tests/routes/_index.test.tsx
@@ -1,10 +1,12 @@
-import { test, expect } from "bun:test";
+import { it, expect, describe } from "bun:test";
 import { screen, render } from "@testing-library/react";
 
 import Index from "~/routes/_index";
 
-test("Can use Testing Library", () => {
-  render(Index());
-  const text = screen.getByText("Welcome to");
-  expect(text).toBeInTheDocument();
+describe("_index", () => {
+  it("should: render component", () => {
+    render(Index());
+    const text = screen.getByText("Join Discord");
+    expect(text).toBeInTheDocument();
+  });
 });
